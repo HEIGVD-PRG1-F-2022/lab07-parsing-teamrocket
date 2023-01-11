0ab620c00dd6a2ef5a29df9256432e08881d6c61
SuperQuentin <quentin.aellen@infrack.ch>
2023-01-11 16:53:40 +0100

    refactor: typo

--- Tunnels.cpp
+++ Tunnels.cpp
@@ -15,37 +15,33 @@ using namespace std;
 Tunnels::Tunnels(const string &file) {
     string line;
     ifstream infile(file);
-    while(getline(infile, line)) {
+    while (getline(infile, line)) {
         if (line.empty()) {
             break;
         }
 
-        string valveName = line.substr(6,2);
+        // Valve name
+        string valveName = line.substr(6, 2);
+        this->valves.push_back(valveName);
 
+        // Valve rate
         short ratePosOffset = 1;
         size_t rateStartPos = line.find('=') + ratePosOffset;
         size_t rateEndPos = line.find(';') - rateStartPos;
-
         unsigned rate = stoi(line.substr(rateStartPos, rateEndPos));
+        this->rates[valveName] = rate;
 
+        // Valve links
         short linksPosOffset = 3;
-
         string linksStrPart = line.substr(line.find("to valve"), line.length());
-
         size_t linksStartPos = linksStrPart.find(" ", 4);
         size_t linksEndPos = linksStrPart.length() - linksStartPos;
+
         vector<string> linksTunnel;
         istringstream split(linksStrPart.substr(linksStartPos, linksEndPos));
-        for(string link; getline(split, link, ','); linksTunnel.push_back(linksStrPart.substr(1, linksStrPart.length())));
-
-
-
+        for (string link; getline(split, link, ','); linksTunnel.push_back(link.substr(1, link.length())));
 
         this->links[valveName] = linksTunnel;
-
-
-
-        this->rates[valveName] = rate;
     }
 }
 
